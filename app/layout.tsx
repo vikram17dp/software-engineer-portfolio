5f4454cfe17825058c69d6dce17e5f0f672b4f41
vikram17dp <vikramdp505@gmail.com>
2026-01-01 16:49:28 +0530

    new commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
+
 import type React from "react"
 import type { Metadata, Viewport } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 import SmoothScroll from "@/components/SmoothScroll"
+import Navigation from "@/components/navigation"
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
